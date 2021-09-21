ff14e649b4394e5a2a70c33c2db176f4854183a5
Julia Noiret <julia.noiret@gmail.com>
2021-09-21 16:08:18 +1000

    project completed

---
+++ src/context.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+
+    // set both to true first to see the result when building the function
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({})
+    const [page, setPage] = useState({page:'', links: []})
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page)
+        setLocation(coordinates)
+        setIsSubmenuOpen(true);
+    }
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return <AppContext.Provider value={{isSubmenuOpen, isSidebarOpen, openSubmenu, openSidebar, closeSubmenu, closeSidebar, location, page}}>{children}</AppContext.Provider>
+}
+
+// custom hook
+export const useGlobalContext = () => {
+    return useContext(AppContext) 
+}
